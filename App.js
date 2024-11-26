ff12a94c69d45f45ba53679d4082145b6e464f74
PradipaDP <kray210104@gmail.com>
2024-11-26 21:53:34 +0700

    Initial commit for P2-LAB-DPM

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Teks di tengah layar */}
+      <Text style={styles.textCenter}>Teks di Tengah Layar</Text>
+      
+      {/* Dua kotak di bawah teks */}
+      <View style={styles.flexContainer}>
+        <View style={[styles.box, styles.boxLeft]} />
+        <View style={[styles.box, styles.boxRight]} />
+      </View>
     </View>
   );
 }
@@ -13,8 +19,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D3D3D3', 
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textCenter: {
+    fontSize: 24, 
+    color: 'blue', 
+    fontWeight: 'bold', 
+    marginBottom: 20,
+  },
+  flexContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    width: '60%',
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  boxLeft: {
+    backgroundColor: 'red', 
+  },
+  boxRight: {
+    backgroundColor: 'green', 
   },
 });
